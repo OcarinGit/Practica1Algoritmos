2911662b3f8b031848361baf306c65838f7e4cc3
JorgeInfante1 <jorge_infante90@hotmail.com>
2019-10-10 15:36:59 -0500

    Hola

--- src/com/practica1algoritmos/app/HolaMundo.java
+++ src/com/practica1algoritmos/app/HolaMundo.java
@@ -5,17 +5,14 @@ public class HolaMundo {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hola Munditu!!!");
-<<<<<<< HEAD
+
 		System.out.println("Esta es otra línea de código");
 		System.out.println("Que pasa Macho");
-=======
+
 		System.out.println("Esta es otra lï¿½nea de cï¿½digo");
 		System.out.println("Dany estuvo aqui");
-<<<<<<< HEAD
 
->>>>>>> ae48016d68f167b06738ffe05364b16262a0078e
-=======
 		System.out.println("Practica de pull, editar, push");
->>>>>>> 294de7573a0689ebf8f8e43129cf5cfc86d93748
+
 	}
 }
